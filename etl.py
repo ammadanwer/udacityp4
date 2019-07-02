7161aac22b729e107013da42fc53db51ccad568e
ammad <ammad.anwer@gmail.com>
2019-07-03 04:12:50 +0500

    Initial commit

---
+++ etl.py
@@ -0,0 +1,92 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+
+
+# config = configparser.ConfigParser()
+# config.read('dl.cfg')
+#
+# os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
+# os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+
+
+def create_spark_session():
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .getOrCreate()
+    return spark
+
+
+def process_song_data(spark, input_data, output_data):
+    # get filepath to song data file
+    song_data = spark.read.json(input_data)
+    # read song data file
+
+    # extract columns to create songs table
+    songs_df = song_data.select('song_id', 'title', 'artist_id', 'year', 'duration')
+    #
+    # write songs table to parquet files partitioned by year and artist
+    songs_df.write.partitionBy('year', 'artist_id').parquet(f'{output_data}songs.pq')
+    # extract columns to create artists table
+    artists_table = song_data.select('artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude')
+    artists_table.show(n=10)
+    # write artists table to parquet files
+    artists_table.write.parquet(f'{output_data}artists.pq')
+
+
+# def process_log_data(spark, input_data, output_data):
+#     # get file path to log data file
+#     log_data =
+#
+#     # read log data file
+#     df =
+#
+#     # filter by actions for song plays
+#     df =
+#
+#     # extract columns for users table
+#     artists_table =
+#
+#     # write users table to parquet files
+#     artists_table
+#
+#     # create timestamp column from original timestamp column
+#     get_timestamp = udf()
+#     df =
+#
+#     # create datetime column from original timestamp column
+#     get_datetime = udf()
+#     df =
+#
+#     # extract columns to create time table
+#     time_table =
+#
+#     # write time table to parquet files partitioned by year and month
+#     time_table
+#
+#     # read in song data to use for songplays table
+#     song_df =
+#
+#     # extract columns from joined song and log datasets to create songplays table
+#     songplays_table =
+#
+#     # write songplays table to parquet files partitioned by year and month
+#     songplays_table
+
+
+def main():
+    spark = create_spark_session()
+    # input_data = "s3://udacity-dend/"
+    input_data = "data/song-data/song_data/*/*/*"
+    output_data = "data/output_data/"
+
+    process_song_data(spark, input_data, output_data)
+    # process_log_data(spark, input_data, output_data)
+
+
+if __name__ == "__main__":
+    main()
